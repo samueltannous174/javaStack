33c256223e92bf8abb1cbb218b3edd5e4ff61ec1
samueltannous <106975094+samueltannous174@users.noreply.github.com>
2025-11-14 20:01:25 +0200

    day11

---
+++ day11/save_travels/src/test/java/org/example/save_travels/SaveTravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.save_travels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveTravelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
