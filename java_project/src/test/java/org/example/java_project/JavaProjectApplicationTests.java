24eb17832732d328748d5811e58b50e3bf8d8baa
samueltannous <106975094+samueltannous174@users.noreply.github.com>
2025-11-25 16:46:06 +0200

    models

---
+++ java_project/src/test/java/org/example/java_project/JavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
